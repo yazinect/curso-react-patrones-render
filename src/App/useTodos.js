d606bc06945bb573c867dc3050f811b21e63cb9b
Yazmin Pedraza <yazminpedraza.it@gmail.com>
2022-07-27 12:55:55 -0500

    Composicion sin react context

--- src/App/useTodos.js
+++ src/App/useTodos.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
-const TodoContext = React.createContext();
-
-function TodoProvider(props) {
+function useTodos() {
   const {
     item: todos,
     saveItem: saveTodos,
@@ -50,9 +48,9 @@ function TodoProvider(props) {
     newTodos.splice(todoIndex, 1);
     saveTodos(newTodos);
   };
-  
+
   return (
-    <TodoContext.Provider value={{
+    {
       loading,
       error,
       totalTodos,
@@ -65,10 +63,8 @@ function TodoProvider(props) {
       deleteTodo,
       openModal,
       setOpenModal,
-    }}>
-      {props.children}
-    </TodoContext.Provider>
+    }
   );
 }
 
-export { TodoContext, TodoProvider };
+export { useTodos };
